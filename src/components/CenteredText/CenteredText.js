6a7c52dbefeb84a455966b6aedc1c6987af15cff
brenda-armero <brendaarmeromartinez@gmail.com>
2023-09-26 11:24:17 +0200

    centeredText

--- src/components/CenteredText/CenteredText.js
+++ src/components/CenteredText/CenteredText.js
@@ -1,21 +1,25 @@
 import { Grid, Typography } from "@mui/material";
+import './CenteredText.css'
 
 export function CenteredText() {
   return (
-    <Grid container direction="row" justifyContent="center" alignItems="center">
-        <Typography variant="body1" align="center" gutterBottom="true">
-            Bienvenido a mi mundo, el emocionante universo del desarrollo web.        
-        </Typography>
-
-        <Typography variant="body1" align="center" gutterBottom="true">
-            El dessarrollo web es un lienzo en blanco esperando a ser diseñado con nuestra creatividad. Es el lugar donde los diseñadores 
-            y desarrolladores pueden unir fuerzas para crear experiencias visuales y funcionales impresionantes.          
-        </Typography>
-
-        <Typography variant="body1" align="center" gutterBottom="true" fontWeight="bold">
-            Soy Brenda y soy una apasionada Front-End Developer con un profundo amor por la creación y la innovación en la web.          
-        </Typography>
-
+    <Grid className='centeredText' container direction="column" justifyContent="space-around" alignItems="center" spacing={2}>
+        <Grid item xs={3}>
+            <Typography variant="body1" align="center" gutterBottom="true">
+                Bienvenido a mi mundo, el emocionante universo del desarrollo web.        
+            </Typography>
+        </Grid>
+        <Grid item xs={3}>
+            <Typography variant="body1" align="center" gutterBottom="true">
+                El dessarrollo web es un lienzo en blanco esperando a ser diseñado con nuestra creatividad. Es el lugar donde los diseñadores 
+                y desarrolladores pueden unir fuerzas para crear experiencias visuales y funcionales impresionantes.          
+            </Typography>
+        </Grid>
+        <Grid item xs={3}>
+            <Typography variant="body1" align="center" gutterBottom="true" fontWeight="bold">
+                Soy Brenda y soy una apasionada Front-End Developer con un profundo amor por la creación y la innovación en la web.          
+            </Typography>
+        </Grid>
     </Grid>
   );
 }
